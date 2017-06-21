64c68412a7217fc136b59152d242584db44429f4
wellwind <wellwind@mail2000.com.tw>
2017-06-21 09:33:38 +0800

    add dyncmic-component directive

--- dynamic-componeny-with-ComponentFactoryResolver/src/app/app.module.ts
+++ dynamic-componeny-with-ComponentFactoryResolver/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { ComponentAComponent } from './component-a/component-a.component';
 import { ComponentBComponent } from './component-b/component-b.component';
 import { ComponentCComponent } from './component-c/component-c.component';
+import { DynamicComponentDirective } from './dynamic-component.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     ComponentAComponent,
     ComponentBComponent,
-    ComponentCComponent
+    ComponentCComponent,
+    DynamicComponentDirective
   ],
   imports: [
     BrowserModule,
