efa0c822f7035d9f02a0f5a735ff122321036ed4
wellwind <wellwind@mail2000.com.tw>
2017-08-10 19:37:10 +0800

    問題：_simulateRequest被重複呼叫

--- using-share-operator/src/app/app.component.ts
+++ using-share-operator/src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { Observer } from 'rxjs/Observer';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+
+  data$: Observable<any>;
+
+  ngOnInit() {
+    this.data$ = this._simulateRequest();
+  }
+
+  private _simulateRequest(): Observable<any> {
+    return Observable.create((observer: Observer<any>) => {
+      console.log('模擬request發生了...');
+      observer.next({ foo: 'bar' });
+      observer.complete();
+    });
+  }
 }
